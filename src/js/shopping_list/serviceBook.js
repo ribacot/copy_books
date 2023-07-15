0b764adb9dd67ea2b716fdb8a6da3d7a1af8e174
ribacot <remontdiskov1979@gmail.com>
2023-07-15 22:57:32 +0300

    Update serviceBook.js

--- src/js/shopping_list/serviceBook.js
+++ src/js/shopping_list/serviceBook.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 export default async function () {
-  const res = await axios(
-    'https://books-backend.p.goit.global/books/category?category=Advice How-To and Miscellaneous'
-  );
-  return res.data;
+  try {
+    const res = await axios(
+      'https://books-backend.p.goit.global/books/category?category=Advice How-To and Miscellaneous'
+    );
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
