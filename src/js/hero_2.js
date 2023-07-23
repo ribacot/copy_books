446966396dd8d17b876a6f730cfbf74dd39bca88
ribacot <remontdiskov1979@gmail.com>
2023-07-23 21:18:47 +0300

    Update hero_2.js

--- src/js/hero_2.js
+++ src/js/hero_2.js
@@ -2,6 +2,7 @@ import { markup } from "./hero";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from "axios";
 import { container } from "./hero";
+
 export default async function () {
   try {
     console.log('axios_in', axios);
@@ -11,7 +12,7 @@ export default async function () {
       `https://books-backend.p.goit.global/books/top-books`
     );
     console.log('after');
-    container.insertAdjacentHTML('beforeend', markup(resp.data));
+   return  container.insertAdjacentHTML('beforeend', markup(resp.data));
   } catch (err) {
     console.log(err);
     Notify.warning('Sorry, failed to load information');
